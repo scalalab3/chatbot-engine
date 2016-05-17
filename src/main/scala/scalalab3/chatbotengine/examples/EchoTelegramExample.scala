a15d7731af7154c1abadec93da6e5017c00cdc06
Andrey Vakunov <a.vakunov@adform.com>
2016-05-17 20:34:49 +0300

    add stubs

---
+++ src/main/scala/scalalab3/chatbotengine/examples/EchoTelegramExample.scala
@@ -0,0 +1,24 @@
+package scalalab3.chatbotengine.examples
+
+import scalalab3.chatbotengine._
+import core._
+import model._
+import scalalab3.chatbotengine.telegram.TelegramLongPoolingEngine
+
+object EchoTelegramExample extends App {
+  val engine = new TelegramLongPoolingEngine()
+  engine.registerChatBot(new EchoChatBot)
+  engine.start()
+}
+
+class EchoChatBot extends ChatBot {
+
+  def receiveMessage(update: Update): Option[OutMessage] = {
+    for {
+      message <- update.message
+    } yield {
+      val chatId = message.chat.id.toString
+      OutMessage(chatId, message.text.getOrElse("Unknown"))
+    }
+  }
+}
