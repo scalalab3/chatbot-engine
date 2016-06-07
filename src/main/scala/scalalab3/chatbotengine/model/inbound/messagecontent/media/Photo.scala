a678fa728fa1d974840fa8cddf6e9491f1417164
muandzip <siarhei.tsitou@syncplicity.com>
2016-06-08 01:10:52 +0300

    fixes in adt and parsing paths

--- src/main/scala/scalalab3/chatbotengine/model/inbound/messagecontent/media/Photo.scala
+++ src/main/scala/scalalab3/chatbotengine/model/inbound/messagecontent/media/Photo.scala
@@ -1,16 +1,9 @@
 package scalalab3.chatbotengine.model.inbound.messagecontent.media
 
-import com.github.tototoshi.play.json.JsonNaming
-import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
-import scalalab3.chatbotengine.model._
-
-case class Photo(photoSizes: Seq[PhotoSize], caption: Option[String] = None) extends MediaContent
+case class Photo(photoSizes: List[PhotoSize]) extends MediaContent
 
 object Photo {
-  implicit lazy val photoReads: Reads[Photo] = JsonNaming.snakecase((
-    (__ \ "photo").readSeqOrEmpty[PhotoSize] and
-      (__ \ "caption").readNullable[String]
-    ) (Photo.apply _))
+    implicit lazy val photoReads: Reads[Photo] = __.read[List[PhotoSize]].map(Photo.apply)
 }
